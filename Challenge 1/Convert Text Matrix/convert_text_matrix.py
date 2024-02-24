8554ac461c069eaa2f9b56aa015efe81a9013308
Henrique Honorio da Silva <contato@henriquesilva.dev>
2024-02-24 16:28:49 -0300

    feat: Challenge 1: Convert Text Matrix

--- Challenge 1/Convert Text Matrix/convert_text_matrix.py
+++ Challenge 1/Convert Text Matrix/convert_text_matrix.py
@@ -4,6 +4,17 @@ from sklearn.feature_extraction.text import CountVectorizer
 target_text = open('datasets/Base Text - Matrix Representation I - v2.txt', 'r').read()
 compared_text = open('datasets/Base Text - Matrix Representation II - v2.txt', 'r').read()
 
+stopwords = open('datasets/stopwords_la.txt', 'r', encoding='utf-8-sig').read()
+
+# Removendo stop words dos textos
+def remove_stop_words(arr_text):
+    words = arr_text.split()
+    filtered_words = [word for word in words if word not in stopwords.split(', ')]
+    return ' '.join(filtered_words)
+
+target_text = remove_stop_words(target_text)
+compared_text = remove_stop_words(compared_text)
+
 # Número de n-gramas
 n = 1
 
